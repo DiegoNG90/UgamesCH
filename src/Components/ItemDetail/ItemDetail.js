262ed0060c2ab933d8910419ec1d58eb4da42438
Diego <diego.nicolas.gandara@gmail.com>
2021-06-17 01:02:24 -0300

    Veo que product llega como array a ItemDetail así que defino variable producto = product[0]

--- src/Components/ItemDetail/ItemDetail.js
+++ src/Components/ItemDetail/ItemDetail.js
@@ -3,24 +3,25 @@ import PropTypes from "prop-types";
 import {Badge, Container, Col, Row, Image} from 'react-bootstrap';
 
 const ItemDetail = ({product}) => {
-console.log("Product desde ItemDetail", product)
+  console.log("Product desde ItemDetail", product[0])
+  const producto = product[0];
 
   return (
       <Container fluid>
         <Row>
           <Col>
-            <h1 id="example-modal-sizes-title-lg"> {product.title + ' '}
+            <h1 id="example-modal-sizes-title-lg"> {producto.title + ' '}
               <Badge variant="light" style={{background:"green"}}> New </Badge> 
             </h1>
             <Container>
                 <Row>
                     <Col sm={6} className="d-flex justify-content-center">
-                        <Image src={product.pictureURL} rounded style={{width: "100%"}} />
+                        <Image src={producto.pictureURL} rounded style={{width: "100%"}} />
                     </Col>
                     <Col sm={6} className="d-flex flex-column justify-content-center">
-                        <p><strong> {product.description} </strong></p>
-                        <p>Price: ${product.price}</p>
-                        <p>Stock: {product.stock} units</p>
+                        <p><strong> {producto.description} </strong></p>
+                        <p>Price: ${producto.price}</p>
+                        <p>Stock: {producto.stock} units</p>
                         <p></p>
                     </Col>
                 </Row>
