a797f16c191a282a66b3ad0b6651d8790ea6e66c
Diego <diego.nicolas.gandara@gmail.com>
2021-06-17 00:51:20 -0300

    Elimino  el Modal y transformo el componente en un simple Container. Además, saco el useParams de acá. Hay un error/bug: el objeto product llega undefined desde ItemDetailContainer...

--- src/Components/ItemDetail/ItemDetail.js
+++ src/Components/ItemDetail/ItemDetail.js
@@ -1,18 +1,15 @@
 import React from "react";
-import { useParams } from 'react-router-dom';
 import PropTypes from "prop-types";
 import {Badge, Container, Col, Row, Image} from 'react-bootstrap';
 
 const ItemDetail = ({product}) => {
-const { id } = useParams();
 console.log("Product desde ItemDetail", product)
-// console.log("Este es el ID desde el componente ItemDetal:", id) // Devuelve undefined
 
   return (
       <Container fluid>
         <Row>
           <Col>
-            <h1 id="example-modal-sizes-title-lg"> {product.title + {id} + ' '} {/* {id} devuelve undefined*/}
+            <h1 id="example-modal-sizes-title-lg"> {product.title + ' '}
               <Badge variant="light" style={{background:"green"}}> New </Badge> 
             </h1>
             <Container>
