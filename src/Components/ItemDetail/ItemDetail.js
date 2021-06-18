460ad24078083e2764d5831abe2d98bf40c5d7ac
Diego <diego.nicolas.gandara@gmail.com>
2021-06-18 18:33:10 -0300

    Refactor de cómo llega el objeto product a través de las props. Se comenta el console.log de testing

--- src/Components/ItemDetail/ItemDetail.js
+++ src/Components/ItemDetail/ItemDetail.js
@@ -3,25 +3,24 @@ import PropTypes from "prop-types";
 import {Badge, Container, Col, Row, Image} from 'react-bootstrap';
 
 const ItemDetail = ({product}) => {
-  console.log("Product desde ItemDetail", product[0])
-  const producto = product[0];
+  // console.log("Product desde ItemDetail", product.length)
 
   return (
       <Container fluid>
         <Row>
           <Col>
-            <h1 id="example-modal-sizes-title-lg"> {producto.title + ' '}
+            <h1 id="example-modal-sizes-title-lg"> {product.title + ' '}
               <Badge variant="light" style={{background:"green"}}> New </Badge> 
             </h1>
             <Container>
                 <Row>
                     <Col sm={6} className="d-flex justify-content-center">
-                        <Image src={producto.pictureURL} rounded style={{width: "100%"}} />
+                        <Image src={product.pictureURL} rounded style={{width: "100%"}} />
                     </Col>
                     <Col sm={6} className="d-flex flex-column justify-content-center">
-                        <p><strong> {producto.description} </strong></p>
-                        <p>Price: ${producto.price}</p>
-                        <p>Stock: {producto.stock} units</p>
+                        <p><strong> {product.description} </strong></p>
+                        <p>Price: ${product.price}</p>
+                        <p>Stock: {product.stock} units</p>
                         <p></p>
                     </Col>
                 </Row>
@@ -29,7 +28,6 @@ const ItemDetail = ({product}) => {
           </Col>
         </Row>     
       </Container>
-
   );
 }
 
