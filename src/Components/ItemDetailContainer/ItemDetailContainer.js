6bf19649e2623804d5503d394aeeb668eca29923
Diego <diego.nicolas.gandara@gmail.com>
2021-06-18 18:55:33 -0300

    Se actualiza la productList para que esté igual a la de ItemListContainer. Se comentan los console.log de testing. Se hace un conditional rendering de ItemDetail para que espere a que el producttenga la key id para renderizarse

--- src/Components/ItemDetailContainer/ItemDetailContainer.js
+++ src/Components/ItemDetailContainer/ItemDetailContainer.js
@@ -11,9 +11,10 @@ const ItemDetailContainer = () => {
     useEffect(() => {
         const productList = [
             {
-                title: "Laptop", 
+                title: "Dell Inspiron 15", 
                 id: 1, 
                 price: 150000, 
+                idCategory: "laptop",
                 stock: 20,
                 description: "Awesome Laptop for programming",
                 pictureURL: "https://images.unsplash.com/photo-1547731030-cd126f44e9c5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80"
@@ -21,7 +22,8 @@ const ItemDetailContainer = () => {
             {
                 title: "Monitor LG 27", 
                 id: 2, 
-                price: 45490, 
+                price: 45490,
+                idCategory: "monitor", 
                 stock: 10,
                 description: "Full color screen with 4K HD graphics",
                 pictureURL: "https://images.unsplash.com/photo-1586952518485-11b180e92764?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=522&q=80"
@@ -29,7 +31,8 @@ const ItemDetailContainer = () => {
             {
                 title: "Gamming chair", 
                 id: 3, 
-                price: 62150, 
+                price: 62150,
+                idCategory: "chair",
                 stock: 15,
                 description: "Most confortable chair for gamming and/or programming",
                 pictureURL: "https://thermaltake.azureedge.net/pub/media/catalog/product/cache/e4fc6e308b66431a310dcd4dc0838059/x/f/xfittwb_01.jpg"
@@ -37,8 +40,9 @@ const ItemDetailContainer = () => {
           ];
 
         const productId = productList.filter(item => item.id == id);
-
-        console.log("ProductID desde ItemDetailContainer",productId)
+        const [filteredProduct] = productId; 
+        // console.log("ProductID desde ItemDetailContainer",productId)
+        // console.log("El id de item/1 es", id) // 1
 
         const fakeRequest = new Promise((resolve, reject) => {
             setTimeout(() => {
@@ -46,7 +50,7 @@ const ItemDetailContainer = () => {
             }, 2000)
             });
         fakeRequest.then((result)=> {
-            setProduct(result)
+            setProduct(filteredProduct)
         }, (error) => {
             throw new Error("Ha habido un error")
         }
@@ -56,10 +60,11 @@ const ItemDetailContainer = () => {
       }, [id])
     
     return (
-        <>
-            <ItemDetail 
-              product={product}
-            />
+        <> { product.hasOwnProperty("id") &&
+                <ItemDetail 
+                product={product}
+                />
+            }
         </>
     )
 }
