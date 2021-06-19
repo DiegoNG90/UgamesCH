85ea3c38ed37ed7a7570029bdd6679a755ed4d68
Diego <diego.nicolas.gandara@gmail.com>
2021-06-19 10:28:42 -0300

    Cambio el método de conteo de los items. Ahora suma la cantidad de cada item.numberOfItems

--- src/Components/CartWidget/CartWidget.js
+++ src/Components/CartWidget/CartWidget.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import PropTypes from "prop-types";
 
 const CartWidget = ({items}) => {
+    const [quantity, setQuantity] = useState(0);
+
+    useEffect(() => {
+        if(items.length>0){
+            let counter = 0;
+            items.forEach((item) => counter += item.numberOfItems)
+            setQuantity(counter)
+        }
+    }, [items])
     return (
-        <i className="fas fa-shopping-cart" style={{ color: "white", fontSize: "1.3rem"}}>{' '+ items.length}</i>
+        <i className="fas fa-shopping-cart" style={{ color: "white", fontSize: "1.3rem"}}>{' '+ quantity}</i> //items.length
     )
 }
 
