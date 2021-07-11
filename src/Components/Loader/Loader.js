448a8aa5ede06c32934cd27308964f4241223c54
Diego <diego.nicolas.gandara@gmail.com>
2021-07-11 14:44:05 -0300

    Creo componente Loader

---
+++ src/Components/Loader/Loader.js
@@ -0,0 +1,18 @@
+import './style.css'
+
+const Loader = () => {
+    return (
+        <div className="main-spinner" >
+            <div className="sk-chase">
+                <div className="sk-chase-dot"></div>
+                <div className="sk-chase-dot"></div>
+                <div className="sk-chase-dot"></div>
+                <div className="sk-chase-dot"></div>
+                <div className="sk-chase-dot"></div>
+                <div className="sk-chase-dot"></div>
+            </div>
+        </div>
+    );
+}
+
+export default Loader;
