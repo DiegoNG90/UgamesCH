fd8e1d12bdb5a0d829c8f2553ca7fdef49542726
Diego <diego.nicolas.gandara@gmail.com>
2021-07-20 15:50:27 -0300

    Le agrego expand y collapese props para que sea un poco más responsivo

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,23 +7,37 @@ import LogoWidget from '../LogoWidget';
 
 const Navbar = () => {
   return (
-    <NavBootstrap bg="dark" variant="dark">
+    <NavBootstrap bg="dark" variant="dark" expand="lg" collapseOnSelect>
       <Container>
         <NavLink activeClassName="selected" to={'/'}>
           <LogoWidget />
         </NavLink>
         <Nav className="mr-auto">
-          <NavLink activeClassName="selected" style={{marginRight: "2em", textDecoration:"none"}} to={'/'}>Home </NavLink>
-          <NavLink activeClassName="selected" style={{marginRight: "2em", textDecoration:"none"}} to={'category/laptop'}>Laptops </NavLink>
+          <NavLink
+            activeClassName="selected"
+            style={{ marginRight: '2em', textDecoration: 'none' }}
+            to={'/'}
+          >
+            Home{' '}
+          </NavLink>
+          <NavLink
+            activeClassName="selected"
+            style={{ marginRight: '2em', textDecoration: 'none' }}
+            to={'category/laptop'}
+          >
+            Laptops{' '}
+          </NavLink>
           {/* <NavLink activeClassName="selected" style={{marginRight: "2em", textDecoration:"none"}} to={'#'}>Contact (WIP)</NavLink> */}
         </Nav>
         <Form className="d-flex justify-content-between">
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button className="ml-xxl-2" variant="outline-info">Search</Button>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button className="ml-xxl-2" variant="outline-info">
+            Search
+          </Button>
         </Form>
-        <CartWidget/>
+        <CartWidget />
       </Container>
-  </NavBootstrap>
+    </NavBootstrap>
   );
 };
 
