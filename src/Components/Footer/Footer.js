aa6d93957f3e6a266cdec7ce1c7b16ce17eab1a6
Diego <diego.nicolas.gandara@gmail.com>
2021-07-20 15:50:51 -0300

    Populo el footer con data del proyecto

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,30 @@
 import './style.css'
+import {Row, Col} from 'react-bootstrap'
+import logo from './logo512.png';
 
 const Footer = () => {
     return (
       <footer className="footer">
-        <p className="footer-paragraph">Este es el footer por ahora </p>
+        <Row className="fila">
+          <h3 className="footer-paragraph">
+            {' '}
+            Site made by Diego Nicolás Gándara - 2021{' '}
+          </h3>
+          <Col
+            className="d-flex justify-content-center align-items-start columna"
+            d-flex
+          >
+            <p className="color">Created with CRA </p>
+            <img src={logo} alt="React Logo" style={{ maxHeight: '25%' }}></img>
+          </Col>
+          <Col className="d-flex justify-content-center">
+            <ul>
+              <li className="color">React-bootstrap</li>
+              <li className="color">React-router-dom</li>
+              <li className="color">Firebase</li>
+            </ul>
+          </Col>
+        </Row>
       </footer>
     );
 }
